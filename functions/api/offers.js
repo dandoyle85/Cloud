d4b945acc06d39db5db92f50656255a5b188918d
dandoyle85 <danny.doyle.85@gmail.com>
2025-09-30 14:22:56 -0400

    Delete functions/api/offers.js

--- functions/api/offers.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const offers = [
-    { network: "ClickBank", name: "Drone Mapping Guide", commission: "50%", signup: "https://www.clickbank.com/" },
-    { network: "Udemy", name: "AI Side Hustle Course", commission: "30%", signup: "https://www.udemy.com/" }
-  ];
-  return new Response(JSON.stringify({ count: offers.length, offers }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
