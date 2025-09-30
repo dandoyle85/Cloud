e24d6c905698c626dcee1799dc5b4d962b10001c
dandoyle85 <danny.doyle.85@gmail.com>
2025-09-30 14:49:32 -0400

    Delete functions/api/sites.js

--- functions/api/sites.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-  const res = await fetch(`${context.env.SUPABASE_URL}/rest/v1/sites`, {
-    headers: {
-      apikey: context.env.SUPABASE_SERVICE_KEY,
-      Authorization: `Bearer ${context.env.SUPABASE_SERVICE_KEY}`
-    }
-  });
-  return new Response(await res.text(), { headers: { "Content-Type": "application/json" } });
-}
